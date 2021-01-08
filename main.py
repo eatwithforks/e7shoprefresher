6b06f8d235efa52fbd26f714187fcc173176cb8a
Jye Lee <jyelee124@gmail.com>
2021-01-07 20:34:39 -0800

    make confidence a constant

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import keyboard
 import random
 
 RANDOM_INTERVAL = random.randint(1,4)
+CONFIDENCE = 0.95
 
 def click(x_loc,y_loc):
     pyautogui.click(clicks=2, interval=0.25, x=x_loc, y=y_loc)
@@ -18,7 +19,7 @@ def buy(type, image_filename, position):
         click(item_button[0] + 800, item_button[1] + 50)
         time.sleep(1) # wait for confirm button
 
-        buy_button_location = pyautogui.locateOnScreen(image_filename, confidence=0.95)
+        buy_button_location = pyautogui.locateOnScreen(image_filename, confidence=CONFIDENCE)
         buy_button=pyautogui.center(buy_button_location)
         pyautogui.moveTo(buy_button[0], buy_button[1])
 
@@ -42,18 +43,18 @@ def refresh_shop(refresh_button):
     click(refresh_button[0], refresh_button[1])
     time.sleep(0.5) # wait for confirm to appear
 
-    confirm_button_pos = pyautogui.locateOnScreen('confirm button.png', confidence=0.95)
+    confirm_button_pos = pyautogui.locateOnScreen('confirm button.png', confidence=CONFIDENCE)
     confirm_button=pyautogui.center(confirm_button_pos)
     click(confirm_button[0], confirm_button[1])
     time.sleep(RANDOM_INTERVAL) # wait for new list to load
 
 while keyboard.is_pressed('q') == False:
-    refresh_button_pos = pyautogui.locateOnScreen('refresh_button.PNG', confidence=0.95)
+    refresh_button_pos = pyautogui.locateOnScreen('refresh_button.PNG', confidence=CONFIDENCE)
     refresh_button = pyautogui.center(refresh_button_pos)
     refresh_button_width = refresh_button_pos[2]
 
-    covenant_pos = pyautogui.locateOnScreen('covenant.png', confidence=0.95)
-    mystic_pos = pyautogui.locateOnScreen('mystic.png', confidence=0.95)
+    covenant_pos = pyautogui.locateOnScreen('covenant.png', confidence=CONFIDENCE)
+    mystic_pos = pyautogui.locateOnScreen('mystic.png', confidence=CONFIDENCE)
     time.sleep(0.5)
 
     # check if bookmarks in initial list
@@ -64,8 +65,8 @@ while keyboard.is_pressed('q') == False:
     scroll_to_bottom(refresh_button)
 
     # check if final item is a bookmark
-    covenant_pos = pyautogui.locateOnScreen('covenant.png', confidence=0.95)
-    mystic_pos = pyautogui.locateOnScreen('mystic.png', confidence=0.95)
+    covenant_pos = pyautogui.locateOnScreen('covenant.png', confidence=CONFIDENCE)
+    mystic_pos = pyautogui.locateOnScreen('mystic.png', confidence=CONFIDENCE)
     buy("covenant", "Buy_button_Covenant.png", covenant_pos)
     buy("mystic", "Buy_button_Mystic.png", mystic_pos)
 
